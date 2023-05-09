31834b5ddb59f7438f0f39471dd4c1ba5127666a
Leon Carvalho <123897359+LeonCarvalhoz@users.noreply.github.com>
2023-05-09 20:18:38 -0300

    Create tde3_server.js

---
+++ tde3_server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const server = express();
+
+
+const port = 3000; 
+
+server.get("/",(req,res)=>{
+    res.send(`server is running`);
+})
+
+server.listen(port, () => {
+    console.log(`server is running on port: ${port}`);
+});
